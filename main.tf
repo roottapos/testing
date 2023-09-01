a7b4731c8358a2d3cfd3d348e22e2d9ed42cce38
roottapos <84537302+roottapos@users.noreply.github.com>
2023-09-01 19:02:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+
+
 locals {
   len_public_subnets      = max(length(var.public_subnets), length(var.public_subnet_ipv6_prefixes))
   len_private_subnets     = max(length(var.private_subnets), length(var.private_subnet_ipv6_prefixes))
